f72f4bc515cd01efc22ea80c307ddf71fc1de9eb
vinay-codess <vinaycodes@zohomail.com>
2026-01-25 14:33:19 -0500

    Update landing page

--- frontend/src/remotion/Root.tsx
+++ frontend/src/remotion/Root.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import { Composition } from 'remotion';
-import { ComparisonComposition } from './ComparisonComposition';
 import { LogComposition } from './LogComposition';
 
 export const RemotionRoot: React.FC = () => {
     return (
         <>
-            <Composition
-                id="Comparison"
-                component={ComparisonComposition}
-                durationInFrames={180}
-                fps={30}
-                width={1280}
-                height={720}
-            />
             <Composition
                 id="Log"
                 component={LogComposition}
